f11f0d467e84533ddaaccee93eaab6da71ffdf00
Andrew Hutchinson <andyhutch77@yahoo.co.uk>
2013-06-28 22:59:22 +0100

    Added the demo MVC application

---
+++ MvcApplication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using OAuthTwitterTimeLine;
+
+namespace MvcApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetTwitterFeed()
+        {
+            var oAuthTwitterTimeline = new OAuthTwitterTimeline();
+            return Json(oAuthTwitterTimeline.GetMyTimeline(), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
