4e58b9c5c1e382a25a2587ca8ca04b87d076ac04
Heather Floyd <hfloyd@scandiaconsulting.com>
2017-04-25 16:02:37 -0400

    New JsonType definition: Place

--- oAuthTwitterWrapper/JsonTypes/TimeLine.cs
+++ oAuthTwitterWrapper/JsonTypes/TimeLine.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-
-namespace OAuthTwitterWrapper.JsonTypes
+﻿namespace OAuthTwitterWrapper.JsonTypes
 {
+    using Newtonsoft.Json;
+    using oAuthTwitterWrapper.JsonTypes;
 
     public class TimeLine
     {
@@ -12,68 +9,68 @@ namespace OAuthTwitterWrapper.JsonTypes
         [JsonProperty("created_at")]
         public string CreatedAt { get; set; }
 
-		[JsonProperty("id")]
-		public string Id { get; set; }
+        [JsonProperty("id")]
+        public string Id { get; set; }
 
-		[JsonProperty("id_str")]
-		public string IdStr { get; set; }
+        [JsonProperty("id_str")]
+        public string IdStr { get; set; }
 
-		[JsonProperty("text")]
-		public string Text { get; set; }
+        [JsonProperty("text")]
+        public string Text { get; set; }
 
-		[JsonProperty("source")]
-		public string Source { get; set; }
+        [JsonProperty("source")]
+        public string Source { get; set; }
 
-		[JsonProperty("truncated")]
-		public bool Truncated { get; set; }
+        [JsonProperty("truncated")]
+        public bool Truncated { get; set; }
 
-		[JsonProperty("in_reply_to_status_id")]
-		public string InReplyToStatusId { get; set; }
+        [JsonProperty("in_reply_to_status_id")]
+        public string InReplyToStatusId { get; set; }
 
-		[JsonProperty("in_reply_to_status_id_str")]
-		public string InReplyToStatusIdStr { get; set; }
+        [JsonProperty("in_reply_to_status_id_str")]
+        public string InReplyToStatusIdStr { get; set; }
 
-		[JsonProperty("in_reply_to_user_id")]
-		public string InReplyToUserId { get; set; }
+        [JsonProperty("in_reply_to_user_id")]
+        public string InReplyToUserId { get; set; }
 
-		[JsonProperty("in_reply_to_user_id_str")]
-		public string InReplyToUserIdStr { get; set; }
+        [JsonProperty("in_reply_to_user_id_str")]
+        public string InReplyToUserIdStr { get; set; }
 
-		[JsonProperty("in_reply_to_screen_name")]
-		public string InReplyToScreenName { get; set; }
+        [JsonProperty("in_reply_to_screen_name")]
+        public string InReplyToScreenName { get; set; }
 
-		[JsonProperty("user")]
-		public User User { get; set; }
+        [JsonProperty("user")]
+        public User User { get; set; }
 
-		[JsonProperty("geo")]
-		public string Geo { get; set; }
+        [JsonProperty("geo")]
+        public string Geo { get; set; }
 
-		[JsonProperty("coordinates")]
-		public string Coordinates { get; set; }
+        [JsonProperty("coordinates")]
+        public string Coordinates { get; set; }
 
-		[JsonProperty("place")]
-		public string Place { get; set; }
+        [JsonProperty("place")]
+        public Place Place { get; set; }
 
-		[JsonProperty("contributors")]
-		public string Contributors { get; set; }
+        [JsonProperty("contributors")]
+        public string Contributors { get; set; }
 
-		[JsonProperty("retweet_count")]
-		public int RetweetCount { get; set; }
+        [JsonProperty("retweet_count")]
+        public int RetweetCount { get; set; }
 
-		[JsonProperty("favorite_count")]
-		public int FavoriteCount { get; set; }
+        [JsonProperty("favorite_count")]
+        public int FavoriteCount { get; set; }
 
-		[JsonProperty("entities")]
-		public Entities Entities { get; set; }
+        [JsonProperty("entities")]
+        public Entities Entities { get; set; }
 
-		[JsonProperty("favorited")]
-		public bool Favorited { get; set; }
+        [JsonProperty("favorited")]
+        public bool Favorited { get; set; }
 
-		[JsonProperty("retweeted")]
-		public bool Retweeted { get; set; }
+        [JsonProperty("retweeted")]
+        public bool Retweeted { get; set; }
 
-		[JsonProperty("lang")]
-		public string Lang { get; set; }
+        [JsonProperty("lang")]
+        public string Lang { get; set; }
     }
 
 }
