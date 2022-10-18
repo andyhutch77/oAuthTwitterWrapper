4ce0bf95938e94dc58c4803ffa3e4304677f10bd
Andrew Hutchinson <andyhutch77@yahoo.co.uk>
2022-10-18 09:23:26 +0100

    upgraded ninject and readme

--- MvcApplication/App_Start/NinjectWebCommon.cs
+++ MvcApplication/App_Start/NinjectWebCommon.cs
@@ -1,61 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using Ninject.Web.Common;
-using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-using Ninject;
-using OAuthTwitterWrapper;
-using MvcApplication.App_Start;
-
-[assembly: WebActivator.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
-[assembly: WebActivator.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(MvcApplication.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(MvcApplication.App_Start.NinjectWebCommon), "Stop")]
 
 namespace MvcApplication.App_Start
 {
-	public static class NinjectWebCommon
-	{
-		private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+    using System;
+    using System.Web;
 
-		/// <summary>
-		/// Starts the application
-		/// </summary>
-		public static void Start()
-		{
-			DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
-			DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-			bootstrapper.Initialize(CreateKernel);
-		}
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
-		/// <summary>
-		/// Stops the application.
-		/// </summary>
-		public static void Stop()
-		{
-			bootstrapper.ShutDown();
-		}
+    using Ninject;
+    using Ninject.Web.Common;
 
-		/// <summary>
-		/// Creates the kernel that will manage your application.
-		/// </summary>
-		/// <returns>The created kernel.</returns>
-		private static IKernel CreateKernel()
-		{
-			var kernel = new StandardKernel();
-			kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
-			kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
-			RegisterServices(kernel);
-			return kernel;
-		}
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
-		/// <summary>
-		/// Load your modules or register your services here!
-		/// </summary>
-		/// <param name="kernel">The kernel.</param>
-		private static void RegisterServices(IKernel kernel)
-		{
-			kernel.Bind<IOAuthTwitterWrapper>().To<OAuthTwitterWrapper.OAuthTwitterWrapper>();
-		}
-	}
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+        }        
+    }
 }
